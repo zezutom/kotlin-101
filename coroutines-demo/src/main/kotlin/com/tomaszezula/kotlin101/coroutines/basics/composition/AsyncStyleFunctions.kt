abc204c7d92088350b85a9c56031953766acbf5d
tomz-upheal <tomas.zezula@upheal.io>
2025-06-24 06:47:27 +0100

    Sequential vs async calls: cancellation.

--- coroutines-demo/src/main/kotlin/com/tomaszezula/kotlin101/coroutines/basics/composition/AsyncStyleFunctions.kt
+++ coroutines-demo/src/main/kotlin/com/tomaszezula/kotlin101/coroutines/basics/composition/AsyncStyleFunctions.kt
@@ -13,23 +13,13 @@ import kotlin.system.measureTimeMillis
  * They are considered "delicate" and require careful management to avoid issues such as resource leaks and unhandled exceptions.
  */
 @OptIn(DelicateCoroutinesApi::class)
-fun callOneAsync(): Deferred<Int> = GlobalScope.async {
+private fun callOneAsync(): Deferred<Int> = GlobalScope.async {
     callA()
 }
 
 @OptIn(DelicateCoroutinesApi::class)
-fun callTwoAsync(): Deferred<Int> = GlobalScope.async {
+private fun callTwoAsync(): Deferred<Int> = GlobalScope.async {
     callB()
-}
-
-fun main() {
-    // This doesn't require a coroutine scope, but it does use GlobalScope.
-    concurrentSumAsync()
-
-    // Let's use a standard approach with structured concurrency for reliable cancellation and resource management.
-    runBlocking {
-        concurrentSum()
-    }
 }
 
 private fun concurrentSumAsync() {
@@ -46,6 +36,28 @@ private fun concurrentSumAsync() {
     println("Concurrent (async) calls completed in $time ms")
 }
 
+@OptIn(DelicateCoroutinesApi::class)
+private fun failedConcurrentSumAsync() {
+    val one = GlobalScope.async {
+        try {
+            delay(5_000)   // Simulate a long-running task
+            42
+        } catch (e: CancellationException) {
+            println("Task one cancelled")
+            throw e
+        } finally {
+            println("Task one finished")
+        }
+    }
+    val two = GlobalScope.async<Int> {
+        println("Task two throws exception")
+        throw ArithmeticException("Simulated failure")
+    }
+    runBlocking {
+        println("The answer is ${one.await() + two.await()}") // This will not be reached
+    }
+}
+
 private suspend fun concurrentSum() = coroutineScope {
     val time = measureTimeMillis {
         val one = async { callA() }
@@ -53,4 +65,51 @@ private suspend fun concurrentSum() = coroutineScope {
         println("The answer is ${one.await() + two.await()}")
     }
     println("Concurrent (coroutine scope) calls completed in $time ms")
+}
+
+private suspend fun failedConcurrentSum() = coroutineScope {
+    val one = async {
+        try {
+            delay(Long.MAX_VALUE) // Simulate a long-running task
+            42
+        } catch (e: CancellationException) {
+            println("Task one cancelled")
+            throw e
+        } finally {
+            println("Task one finished")
+        }
+    }
+    val two = async<Int> {
+        println("Task two throws an exception")
+        throw ArithmeticException("Simulated failure")
+    }
+    println("The answer is ${one.await() + two.await()}") // This will not be reached
+}
+
+fun main() {
+    // This doesn't require a coroutine scope, but it does use GlobalScope.
+    concurrentSumAsync()
+
+    // Let's use a standard approach with structured concurrency for reliable cancellation and resource management.
+    runBlocking {
+        concurrentSum()
+    }
+
+    // Structured concurrency takes care of cancellation and resource management.
+    runBlocking {
+        try {
+            failedConcurrentSum()
+        } catch (e: Exception) {
+            println("Calculation failed with exception: ${e.message}")
+        }
+    }
+
+    // Global scope won't handle cancellation and leads to resource leak.
+    runBlocking {
+        try {
+            failedConcurrentSumAsync()
+        } catch (e: Exception) {
+            println("Async calculation failed with exception: ${e.message}")
+        }
+    }
 }
